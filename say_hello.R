ce710459a865f0ca847764d49855879d488c5407
Alistair Inglis <alistair-inglis@dfid.gov.uk>
2020-05-12 14:53:47 +0100

    commenting our line 2 - trying to create merge conflict

--- say_hello.R
+++ say_hello.R
@@ -1,5 +1,5 @@
 say_hello <- function() {
-  print('Nathan: Hello!')
+  #print('Nathan: Hello!')
   print('Alistair: Hello! How about this weather, huh?')
   print('Nathan: Your conversational skills are excellent!')
   print('Alistair: Just unlike my git skills!')
