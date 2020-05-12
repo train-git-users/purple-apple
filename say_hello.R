a1f5580ee2b861dc06f7e39cdd7c0fba7574e794
Alistair Inglis <alistair-inglis@dfid.gov.uk>
2020-05-12 14:47:55 +0100

    changed dialogue

--- say_hello.R
+++ say_hello.R
@@ -1,8 +1,8 @@
 say_hello <- function() {
   print('Nathan: Hello!')
   print('Alistair: Hello! How about this weather, huh?')
-  print('Nathan: Your conversational skills need improving')
-  print('Alistair: Just like my git skills!')
+  print('Nathan: Your conversational skills are excellent!')
+  print('Alistair: Just unlike my git skills!')
   print('Nathan: Ha-ha!')
 }
 
