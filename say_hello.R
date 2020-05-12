3c3696acd466a23c0bba1a2eb8dc6f55177dd6f6
nathanbegbie <nathanbegbie@gmail.com>
2020-05-12 14:56:01 +0100

    push directly to the master branch like the sinner that I am

--- say_hello.R
+++ say_hello.R
@@ -1,7 +1,7 @@
 say_hello <- function() {
   print('Nathan: Hello!')
   print('Alistair: Hello! How about this weather, huh?')
-  print('Nathan: Your conversational skills need improving')
+  print('Nathan: Your conversational skills are great')
   print('Alistair: Just like my git skills!')
   print('Nathan: Ha-ha!')
 }
